c61652e51927f7b4c48fd1c77b4069cf16578d8e
Kayle Patton <kayle.patton@bbc.co.uk>
2022-12-30 12:55:08 +0000

    fix books entry field

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,10 @@
-import Box from "@mui/material/Box";
 import { Title } from "../components/Title";
 
 export const LandingPage = () => {
+
   return <>
       <Title title="LANDING PAGE TITLE" />
+
     </>
 
 };
