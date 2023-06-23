b83f00b3015e97844db119528bf13cc79b7b8d6c
Kayle Patton <kayle.patton@bbc.co.uk>
2023-06-23 13:37:58 +0100

    restructure logo group

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import IconButton from "@mui/material/IconButton";
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { ReactComponent as LogoIcon } from '../peculiar-bird-icon.svg'
 
 import { useMediaQuery } from 'react-responsive';
-
 
 export const Footer = () => {
   const mobile = useMediaQuery({ query: "(max-width: 499px" });
@@ -24,32 +24,51 @@ export const Footer = () => {
         aria-label="email"
         href="mailto:kayle.patton22@gmail.com"
         target="_blank">
-        <EmailIcon fontSize='large'/>
+        <EmailIcon fontSize='large' />
       </IconButton>
       <IconButton
         aria-label="github"
         href="https://github.com/kayleriegerpatton"
         target="_blank" >
-        <GitHubIcon fontSize='large'/>
+        <GitHubIcon fontSize='large' />
       </IconButton>
       <IconButton
         aria-label="linkedin"
         href="https://www.linkedin.com/in/kaylerieger/"
         target="_blank">
-        <LinkedInIcon fontSize='large'/>
+        <LinkedInIcon fontSize='large' />
       </IconButton>
     </Stack>
 
-    <Stack 
-    direction={mobile ? "column" : "row"}
-    spacing={2}
-    alignItems="center"
-    justifyContent="center"
-    marginBottom="2rem"
+    <Stack
+      direction={mobile ? "column" : "row"}
+      spacing={2}
+      alignItems="center"
+      justifyContent="center"
+      marginBottom="2rem"
     >
       <a href='/'>Home</a> <a href='/about'>About</a> <a href='/signup'>Sign Up</a>
     </Stack>
 
-    <Typography marginTop="20px">{`©${year} Kayle Rieger Patton `}</Typography>
+{/* Logo Group */}
+    <Stack
+      direction={mobile ? "column" : "row"}
+      alignItems="center"
+      justifyContent="center"
+      spacing={0.5}>
+      <LogoIcon fill='white' width="6rem" height="6rem" />
+      <div>
+        <Typography
+          fontFamily={'Amatic SC'}
+          fontWeight={'700'}
+          fontSize="3.125rem"
+          color='white'
+          >
+            {`The Peculiar Project`}
+        </Typography>
+        <Typography fontFamily={'Libre Franklin'} color='white'>{`Copyright © ${year} Kayle Rieger Patton `}</Typography>
+      </div>
+    </Stack>
+
   </footer>;
 };
