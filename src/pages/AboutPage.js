926ed66866bc54d22665b285b002fc6b8e3e309c
Kayle Patton <kayle.patton@bbc.co.uk>
2023-08-04 12:35:37 +0100

    about page developer section

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,4 +1,4 @@
-import { Button, IconButton, Stack, Typography } from "@mui/material"
+import { Avatar, Button, IconButton, Stack, Typography } from "@mui/material"
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -18,7 +18,7 @@ export const AboutPage = () => {
             direction="row"
             spacing={1}
             justifyContent="left"
-            >
+          >
             <Stack
               direction="column"
             >
@@ -26,61 +26,84 @@ export const AboutPage = () => {
                 aria-label="github"
                 href="https://github.com/kayleriegerpatton/peculiar-ui"
                 target="_blank"
-                sx={{paddingBottom: "0px"}}
+                sx={{ marginBottom: "-5px" }}
               >
                 <GitHubIcon sx={{ fontSize: "3rem" }} />
               </IconButton>
-              <p className="logo-text">UI</p>
+              <a className="logo-text" href="mailto:kayle.patton22@gmail.com" target="_blank" rel="noreferrer">UI</a>
             </Stack>
             <Stack>
               <IconButton
                 aria-label="github"
                 href="https://github.com/kayleriegerpatton/peculiar-api"
                 target="_blank"
-                sx={{paddingBottom: "0px"}}
+                sx={{ marginBottom: "-5px" }}
               >
                 <GitHubIcon sx={{ fontSize: "3rem" }} />
               </IconButton>
-              <p className="logo-text">API</p>
+              <a className="logo-text" href="mailto:kayle.patton22@gmail.com" target="_blank" rel="noreferrer">API</a>
             </Stack>
           </Stack>
         </div>
       </div>
 
-      <div>
-        <h2 className="page-title">About the Developer</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <img alt="Kayle wearing a black blazer and glasses." src="https://peculiar-project-assets.s3.eu-west-1.amazonaws.com/profile.jpeg"></img>
-        <Stack direction="row">
-          <IconButton
-            aria-label="github"
-            href="https://github.com/kayleriegerpatton"
-            target="_blank"
-          >
-            <GitHubIcon fontSize='large' />
-          </IconButton>
+      <div className="about-container">
+        <div className="about-text">
+          <h2 className="page-title">About the Developer</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p>Get in Touch:</p>
 
-          <IconButton
-            aria-label="linkedin"
-            href="https://www.linkedin.com/in/kaylerieger/"
-            target="_blank">
-            <LinkedInIcon fontSize='large' />
-          </IconButton>
+          <Stack
+            direction="row"
+            spacing={2}
+            justifyContent="left">
+            <Stack>
+              <IconButton
+                aria-label="github"
+                href="https://github.com/kayleriegerpatton"
+                target="_blank"
+                sx={{ marginBottom: "-5px" }}
+              >
+                <GitHubIcon sx={{ fontSize: "3rem" }} />
+              </IconButton>
+              <a className="logo-text" href="mailto:kayle.patton22@gmail.com" target="_blank" rel="noreferrer">GitHub</a>
+            </Stack>
+            <Stack>
+              <IconButton
+                aria-label="linkedin"
+                href="https://www.linkedin.com/in/kaylerieger/"
+                target="_blank"
+                sx={{ marginBottom: "-5px" }}>
+                <LinkedInIcon sx={{ fontSize: "3rem" }} />
+              </IconButton>
+              <a className="logo-text" href="mailto:kayle.patton22@gmail.com" target="_blank" rel="noreferrer">LinkedIn</a>
+            </Stack>
+            <Stack>
+              <IconButton
+                aria-label="portfolio"
+                href="http://www.tinyurl.com/krp-portfolio"
+                target="_blank"
+                sx={{ marginBottom: "-5px" }}>
+                <WebIcon sx={{ fontSize: "3rem" }} />
+              </IconButton>
+              <a className="logo-text" href="mailto:kayle.patton22@gmail.com" target="_blank" rel="noreferrer">Portfolio</a>
+            </Stack>
+            <Stack>
+              <IconButton
+                aria-label="email"
+                href="mailto:kayle.patton22@gmail.com"
+                target="_blank"
+                sx={{ marginBottom: "-5px" }}>
+                <EmailIcon sx={{ fontSize: "3rem" }} />
+              </IconButton>
+              <a className="logo-text" href="mailto:kayle.patton22@gmail.com" target="_blank" rel="noreferrer">Email</a>
+            </Stack>
+          </Stack>
+        </div>
+        <div>
+          <img alt="Kayle wearing a black blazer and glasses." src="https://peculiar-project-assets.s3.eu-west-1.amazonaws.com/profile.jpeg"></img>
+        </div>
 
-          <IconButton
-            aria-label="portfolio"
-            href="http://www.tinyurl.com/krp-portfolio"
-            target="_blank" >
-            <WebIcon fontSize='large' />
-          </IconButton>
-
-          <IconButton
-            aria-label="email"
-            href="mailto:kayle.patton22@gmail.com"
-            target="_blank">
-            <EmailIcon fontSize='large' />
-          </IconButton>
-        </Stack>
       </div>
     </div>
   )
