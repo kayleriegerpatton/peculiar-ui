dacbcb2b6f72e716b3c124aa3b489db9f77048c8
Kayle Patton <kayle.patton@bbc.co.uk>
2023-08-04 12:46:25 +0100

    adjust text styling

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,7 +7,7 @@ import { ReactComponent as LogoIcon } from '../peculiar-bird-icon.svg'
 
 export const AboutPage = () => {
   return (
-    <div className="about-page">
+    <>
       <div className="about-container">
         <LogoIcon fill='var(--dark-liver)' width="22rem" height="" id="about-logo" />
         <div className="about-text">
@@ -47,64 +47,66 @@ export const AboutPage = () => {
         </div>
       </div>
 
-      <div className="about-container">
-        <div className="about-text">
-          <h2 className="page-title">About the Developer</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <p>Get in Touch:</p>
+        <div className="about-container">
+          <div className="about-text">
+            <h2 className="page-title">About the Developer</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Get in Touch:</p>
 
-          <Stack
-            direction="row"
-            spacing={2}
-            justifyContent="left">
-            <Stack>
-              <IconButton
-                aria-label="github"
-                href="https://github.com/kayleriegerpatton"
-                target="_blank"
-                sx={{ marginBottom: "-5px" }}
-              >
-                <GitHubIcon sx={{ fontSize: "3rem" }} />
-              </IconButton>
-              <a className="logo-text" href="mailto:kayle.patton22@gmail.com" target="_blank" rel="noreferrer">GitHub</a>
+            <Stack
+              direction="row"
+              spacing={2}
+              justifyContent="left">
+              <Stack>
+                <IconButton
+                  aria-label="github"
+                  href="https://github.com/kayleriegerpatton"
+                  target="_blank"
+                  sx={{ marginBottom: "-5px" }}
+                >
+                  <GitHubIcon sx={{ fontSize: "3rem" }} />
+                </IconButton>
+                <a className="logo-text" href="https://github.com/kayleriegerpatton" target="_blank" rel="noreferrer">GitHub</a>
+              </Stack>
+              <Stack>
+                <IconButton
+                  aria-label="linkedin"
+                  href="https://www.linkedin.com/in/kaylerieger/"
+                  target="_blank"
+                  sx={{ marginBottom: "-5px" }}>
+                  <LinkedInIcon sx={{ fontSize: "3rem" }} />
+                </IconButton>
+                <a className="logo-text" href="https://www.linkedin.com/in/kaylerieger/" target="_blank" rel="noreferrer">LinkedIn</a>
+              </Stack>
+              <Stack>
+                <IconButton
+                  aria-label="portfolio"
+                  href="http://www.tinyurl.com/krp-portfolio"
+                  target="_blank"
+                  sx={{ marginBottom: "-5px" }}>
+                  <WebIcon sx={{ fontSize: "3rem" }} />
+                </IconButton>
+                <a className="logo-text" href="http://www.tinyurl.com/krp-portfolio" target="_blank" rel="noreferrer">Portfolio</a>
+              </Stack>
+              <Stack>
+                <IconButton
+                  aria-label="email"
+                  href="mailto:kayle.patton22@gmail.com"
+                  target="_blank"
+                  sx={{ marginBottom: "-5px" }}>
+                  <EmailIcon sx={{ fontSize: "3rem" }} />
+                </IconButton>
+                <a className="logo-text" href="mailto:kayle.patton22@gmail.com" target="_blank" rel="noreferrer">Email</a>
+              </Stack>
             </Stack>
-            <Stack>
-              <IconButton
-                aria-label="linkedin"
-                href="https://www.linkedin.com/in/kaylerieger/"
-                target="_blank"
-                sx={{ marginBottom: "-5px" }}>
-                <LinkedInIcon sx={{ fontSize: "3rem" }} />
-              </IconButton>
-              <a className="logo-text" href="mailto:kayle.patton22@gmail.com" target="_blank" rel="noreferrer">LinkedIn</a>
-            </Stack>
-            <Stack>
-              <IconButton
-                aria-label="portfolio"
-                href="http://www.tinyurl.com/krp-portfolio"
-                target="_blank"
-                sx={{ marginBottom: "-5px" }}>
-                <WebIcon sx={{ fontSize: "3rem" }} />
-              </IconButton>
-              <a className="logo-text" href="mailto:kayle.patton22@gmail.com" target="_blank" rel="noreferrer">Portfolio</a>
-            </Stack>
-            <Stack>
-              <IconButton
-                aria-label="email"
-                href="mailto:kayle.patton22@gmail.com"
-                target="_blank"
-                sx={{ marginBottom: "-5px" }}>
-                <EmailIcon sx={{ fontSize: "3rem" }} />
-              </IconButton>
-              <a className="logo-text" href="mailto:kayle.patton22@gmail.com" target="_blank" rel="noreferrer">Email</a>
-            </Stack>
-          </Stack>
-        </div>
-        <div>
-          <img alt="Kayle wearing a black blazer and glasses." src="https://peculiar-project-assets.s3.eu-west-1.amazonaws.com/profile.jpeg"></img>
+          </div>
+          <div>
+            <img alt="Kayle wearing a black blazer and glasses." src="https://peculiar-project-assets.s3.eu-west-1.amazonaws.com/profile.jpeg"></img>
+          </div>
+
         </div>
 
-      </div>
-    </div>
+
+    </>
   )
 }
