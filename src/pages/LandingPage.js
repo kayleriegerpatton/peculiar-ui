1371d480be49d0489f9033de90f0ccf9aa130be6
Kayle Patton <kayle.patton@bbc.co.uk>
2023-11-03 12:13:31 +0000

    style landing page first section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,28 +4,33 @@ import PublicIcon from '@mui/icons-material/Public';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
+import { styles } from "../styles";
+import "./LandingPage.css"
+
 export const LandingPage = () => {
 
   return (
     <>
-      <Title title="LANDING PAGE TITLE" />
+      {/* <Title title="LANDING PAGE TITLE" /> */}
       <div className="landing-section-background">
         <div className="landing-container">
-          <h2>Extraordinary things began to happen...</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <Button>Get peculiar</Button>
-        </div>
-        <div>
-          <img alt="" src="https://peculiar-project-assets.s3.eu-west-1.amazonaws.com/ymbryne-council-symbol.jpeg"></img>
+          <div className="landing-text">
+            <h2 className="page-title">Extraordinary things began to happen...</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <Button sx={styles.button}>Get Peculiar</Button>
+          </div>
+          {/* <div> */}
+            <img alt="" src="https://picsum.photos/650/500"></img>
+          {/* </div> */}
         </div>
       </div>
 
       <div className="landing-container">
-        <h2>Become Part of Peculiardom</h2>
+        <h2 className="page-title">Become Part of Peculiardom</h2>
         <Stack
-        direction="row"
-        spacing={20}
-        justifyContent="center">
+          direction="row"
+          spacing={20}
+          justifyContent="center">
           <Stack>
             <PersonAddIcon sx={{ fontSize: "3rem" }} />
             <p>Explore Characters</p>
